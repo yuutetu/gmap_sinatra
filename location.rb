30783b1cb9770c0a4d8a2c105d0fc42784bfad2c
Yuto Ogi <jacoyutorius@gmail.com>
2012-11-14 16:26:29 +0900

    Modified location.rb delete old codle

--- location.rb
+++ location.rb
@@ -22,9 +22,3 @@ class Location
 	end
 end
 
-def getFriendLocation
-	l = Location.new("tokyo",35.33386,139.404716)
-	l2 = Location.new("nagoya",35.181446, 136.906398)
-	l3 = Location.new("anjo",34.958641,137.080297)
-	return [l,l2,l3]
-end
